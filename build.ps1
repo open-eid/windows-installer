ee5e4ac01aef36014bd276fea5ed98b671166e82
Raul Metsma <raul@metsma.ee>
2015-09-07 22:50:20 +0300

    Sign packages

--- build.ps1
+++ build.ps1
@@ -5,7 +5,8 @@ param(
    [string]$light = "$env:WIX\bin\light.exe",
    [string]$torch = "$env:WIX\bin\torch.exe",
    [string]$msbuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe",
-   [string]$version = "3.12.0.0"
+   [string]$version = "3.12.0.0",
+   [string]$sign = $null
 )
 
 $env:MSI_VERSION = $version
@@ -18,26 +19,32 @@ if( !(Test-Path Env:\VCINSTALLDIR) ) {
 }
 $filename = "Eesti_ID_kaart-$version$env:VER_SUFFIX"
 
+Function Sign ($filename) {
+    signtool.exe sign /a /v /s MY /n "$sign" /fd SHA256 /du http://installer.id.ee `
+        /t http://timestamp.verisign.com/scripts/timstamp.dll "$filename"
+}
+
 & $msbuild "/p:Configuration=Release;Platform=Win32" EstEIDFindCertificateAction\EstEIDFindCertificateAction.vcxproj
 
 $cwd = Split-Path $MyInvocation.MyCommand.Path
 $shell = new-object -com shell.application
 
 mkdir -Force -Path "libs"
+mkdir -Force -Path "unsigned"
 msiexec /qn /a Digidoc3_Client.msi TARGETDIR="$cwd\libs" | Out-Null
 msiexec /qn /a ID-card_utility.msi TARGETDIR="$cwd\libs" | Out-Null
 msiexec /qn /a chrome-token-signing.msi TARGETDIR="$cwd\libs" | Out-Null
 msiexec /qn /a esteid-plugin-ie_x64.msi TARGETDIR="$cwd\X64" | Out-Null
 msiexec /qn /a esteid-plugin-ie_x86.msi TARGETDIR="$cwd\X86" | Out-Null
 
-
 Remove-Item "$env:CLIENT_PATH\certs\TEST*.crt"
-& $heat dir "$env:CLIENT_PATH\certs" -gg -scom -sreg -sfrag -srd -cg SKCertificates `
+& $heat dir "$env:CLIENT_PATH\certs" -nologo -gg -scom -sreg -sfrag -srd -cg SKCertificates `
   -dr CertificatesFolder -var var.certsLocation -out CertsFragment.wxs
 
 $wix_modules = @("WixUI_IDCard", "WixUI_ChangeLog", "RemoveOldSoftDlg", "MyInstallDirDlg",
     "minidriver", "opensc", "commonlibs", "CertsFragment", "qdigidoc", "qesteidutil", "updater",
     "esteid-browser-plugin", "ie-plugin", "chrome-token-signing", "Eesti_ID_kaart")
+$cultures = @("en-US", "et-EE")
 
 foreach($platform in @("x86", "x64")) {
     foreach($version in @("7", "8", "8.1")) {
@@ -50,15 +57,18 @@ foreach($platform in @("x86", "x64")) {
         & $candle -nologo "$mod.wxs" -v -ext WixUtilExtension -ext WixDifxAppExtension `
           "-dPlatform=$platform" "-dcertsLocation=$env:CLIENT_PATH\certs"
     }
-    $cultures = @("en-US", "et-EE")
     foreach($culture in $cultures) {
         $wix_modules_obj = ($wix_modules -join ".wixobj ") + ".wixobj"
-        & $light -nologo -out "$($filename)_$culture.$platform.msi" -v -sice:ICE57 -sice:ICE03 `
+        & $light -nologo -out "$($filename)_$culture.$platform.msi" -sice:ICE57 -sice:ICE03 `
           -ext WixUtilExtension -ext WixUIExtension -ext WixDifxAppExtension `
           -loc IDCard_$culture.wxl -cultures:$culture `
           "-dcertsLocation=$env:CLIENT_PATH\certs" `
           "$env:WIX\bin\difxapp_$platform.wixlib" `
           $wix_modules_obj.Split(" ")
+        if($sign) {
+            cp "$($filename)_$culture.$platform.msi" "unsigned"
+            Sign("$($filename)_$culture.$platform.msi")
+        }
     }
     & $torch -nologo -p -t language -out `
       "$($filename)_$($cultures[1]).$platform.mst" `
