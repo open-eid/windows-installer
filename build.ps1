d7515bee0ea75f4d7e51ddc19d17c66659738b14
Raul Metsma <raul@metsma.ee>
2016-02-18 17:12:44 +0200

    Option to install X32 bit desktop applications
    IB-4338

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ param(
    [string]$insignia = "$env:WIX\bin\insignia.exe",
    [string]$msiversion = "3.12.0.0",
    [string]$url = "https://installer.id.ee/media/windows/{2}",
-   [string]$filename = "Open-EID-$msiversion$env:VER_SUFFIX.exe",
+   [string]$filename = "Open-EID-$msiversion$env:VER_SUFFIX",
    [string]$updater = "ID-Updater",
    [string]$qdigidoc = "Digidoc3_Client",
    [string]$qesteid = "ID-card_utility",
@@ -31,6 +31,7 @@ Function GetBaseName($find, $substring) {
     return $find
 }
 
+$path = split-path -parent $MyInvocation.MyCommand.Definition
 $qdigidoc = GetBaseName $qdigidoc 10
 $qesteid = GetBaseName $qesteid 10
 $updater = GetBaseName $updater 4
@@ -40,29 +41,32 @@ $firefox = GetBaseName $firefox 4
 $loader = GetBaseName $loader 4
 $minidriver = GetBaseName $minidriver 4
 
-Function Sign ($filename) {
+Function Sign($filename) {
     signtool.exe sign /a /v /s MY /n "$sign" /fd SHA256 /du http://installer.id.ee `
         /t http://timestamp.verisign.com/scripts/timstamp.dll "$filename"
 }
+Function Create($filename, $defaultX64) {
+    & $candle "$path\bootstrapper.wxs" -nologo -ext WixBalExtension -ext WixUtilExtension `
+        "-dMSI_VERSION=$msiversion" "-dpath=$path" "-ddefaultX64=$defaultX64" "-dURL=$url" "-dembed=$embed" `
+        "-dupdater=$updater" "-dqesteidutil=$qesteid" "-dqdigidoc=$qdigidoc" "-dminidriver=$minidriver" `
+        "-dieplugin=$ieplugin" "-dchrome=$chrome" "-dfirefox=$firefox" "-dloader=$loader"
+    & $light bootstrapper.wixobj -nologo -ext WixBalExtension -out "$filename"
+    if($sign) {
+        cp "$filename" "unsigned"
+        & $insignia -nologo -ib "$filename" -o "$filename.engine.exe"
+        Sign("$filename.engine.exe")
+        & $insignia -nologo -ab "$filename.engine.exe" "$filename" -o "$filename"
+        Sign("$filename")
+    }
+}
 
-& $candle -nologo metainfo.wxs "-dMSI_VERSION=$msiversion"
+& $candle -nologo "$path\metainfo.wxs" "-dMSI_VERSION=$msiversion"
 & $light -nologo -out "metainfo.msi" metainfo.wixobj
-& $candle -nologo uninstaller.wxs "-dMSI_VERSION=$msiversion"
+& $candle -nologo "$path\uninstaller.wxs" "-dMSI_VERSION=$msiversion"
 & $light -nologo -out "uninstaller.msi" uninstaller.wixobj
 if($sign) {
     Sign("metainfo.msi")
     Sign("uninstaller.msi")
 }
-
-& $candle bootstrapper.wxs -nologo -ext WixBalExtension -ext WixUtilExtension "-dMSI_VERSION=$msiversion" `
-    "-dupdater=$updater" "-dqesteidutil=$qesteid" "-dqdigidoc=$qdigidoc" "-dminidriver=$minidriver" `
-    "-dieplugin=$ieplugin" "-dchrome=$chrome" "-dfirefox=$firefox" "-dloader=$loader" "-dURL=$url" "-dembed=$embed"
-& $light bootstrapper.wixobj -nologo -ext WixBalExtension -out "$filename"
-if($sign) {
-    cp "$filename" "unsigned"
-    & $insignia -nologo -ib "$filename" -o engine.exe
-    Sign("engine.exe")
-    & $insignia -nologo -ab engine.exe "$filename" -o "$filename"
-    Sign("$filename")
-}
-
+Create $filename".exe" 1
+Create $filename"_x86.exe" 0
