8cfddef417138eafef3c7a41de777ddb8d38874b
Raul Metsma <raul@metsma.ee>
2017-07-19 11:52:01 +0300

    Remove this option, workaround broke upgrades (#18)
    IB-4843
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- RunQesteidutil.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-if [%1] NEQ [1] goto :eof
-IF EXIST "C:\Program Files\Open-EID\qesteidutil.exe" (start "" "C:\Program Files\Open-EID\qesteidutil.exe")
-IF EXIST "C:\Program Files (x86)\Open-EID\qesteidutil.exe" (start "" "C:\Program Files (x86)\Open-EID\qesteidutil.exe")
